e1ae8cad6a6affb31d27269e55a2ac3f52ac43a8
PatukariSaiKiran <patukarisaikiran0412@gmail.com>
2026-03-24 17:23:03 +0000

    Initial project setup with structure

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-3xl font-bold text-blue-500">
+      Tailwind is working
+    </div>
+  );
+}
+export default App;
