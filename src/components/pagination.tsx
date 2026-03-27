5bb9ce6c9432962327ce7ce312e51c0e89ce66d2
PatukariSaiKiran <patukarisaikiran0412@gmail.com>
2026-03-27 13:08:58 +0000

    feat: add pagination UI with arrow controls and improve product listing

---
+++ src/components/pagination.tsx
@@ -0,0 +1,53 @@
+import LeftArrow from '../assets/icons/arrow-left.svg?react';
+import RightArrow from '../assets/icons/arrow-right.svg?react';
+
+type PaginationProps = {
+  currentPage: number;
+  totalItems: number;
+  itemsPerPage: number;
+  onPageChange: (page: number) => void;
+};
+
+function Pagination({
+  currentPage,
+  totalItems,
+  itemsPerPage,
+  onPageChange,
+}: PaginationProps) {
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  if (totalPages <= 1) return null;
+
+  const endItem = Math.min(currentPage * itemsPerPage, totalItems);
+
+  return (
+    <div className="mt-8 flex items-center justify-center gap-4 text-lg">
+
+      {/* LEFT */}
+      <button
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+        className="p-2 border rounded-md disabled:opacity-40 hover:bg-gray-100 transition"
+      >
+        <LeftArrow className="w-5 h-5" />
+      </button>
+
+      {/* TEXT */}
+      <span className="text-gray-700 font-medium">
+        {endItem} / {totalItems}
+      </span>
+
+      {/* RIGHT */}
+      <button
+        onClick={() => onPageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+        className="p-2 border rounded-md disabled:opacity-40 hover:bg-gray-100 transition"
+      >
+        <RightArrow className="w-5 h-5" />
+      </button>
+
+    </div>
+  );
+}
+
+export default Pagination;
